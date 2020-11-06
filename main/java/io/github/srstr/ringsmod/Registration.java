e464b226cb5025b6349ef91419529b1ebffb944c
sstrac <s.r.strachan@outlook.com>
2020-11-06 02:29:57 +0000

    grand gravel rework, register burnt_ice

--- main/java/io/github/srstr/ringsmod/Registration.java
+++ main/java/io/github/srstr/ringsmod/Registration.java
@@ -1,18 +1,16 @@
 package io.github.srstr.ringsmod;
 
+import io.github.srstr.ringsmod.block.BurntIceBlock;
 import io.github.srstr.ringsmod.block.GrandGravelBlock;
 import io.github.srstr.ringsmod.entity.RisingBlockEntity;
 import io.github.srstr.ringsmod.item.FireRing;
 import io.github.srstr.ringsmod.item.OceanRing;
 import net.minecraft.block.Block;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.item.FallingBlockEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.util.registry.Registry;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -25,7 +23,9 @@ public class Registration {
     public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RingsMod.MOD_ID);
     public static DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, RingsMod.MOD_ID);
     public static DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, RingsMod.MOD_ID);
-    public static final EntityType<RisingBlockEntity> RISING_BLOCK_ENTITY = EntityType.Builder.<RisingBlockEntity>create(RisingBlockEntity::new, EntityClassification.MISC).size(0.98F, 0.98F).func_233606_a_(10).func_233608_b_(20).build("rising_block");
+    public static final EntityType<RisingBlockEntity> RISING_BLOCK_ENTITY = EntityType.Builder.<RisingBlockEntity>create(RisingBlockEntity::new, EntityClassification.MISC).build("rising_block");
+    //.size(0.98F, 0.98F).func_233606_a_(10).func_233608_b_(20).
+
 
     public static void registerAll(){
         ITEMS.register("fire_ring_1", FireRing::create);
@@ -37,6 +37,7 @@ public class Registration {
         ITEMS.register("ocean_ring_3", () -> OceanRing.create(3));
         ENTITIES.register("rising_block_entity", () -> RISING_BLOCK_ENTITY);
         registerBlock("grand_gravel", GrandGravelBlock::create);
+        registerBlock("burnt_ice", BurntIceBlock::create);
         //registerEnergy("white_fire", RingsmodBlocks.WHITE_FIRE);
     }
 
