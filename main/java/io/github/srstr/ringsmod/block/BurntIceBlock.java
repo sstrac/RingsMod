557c04a2621743d6605db3936222ffdbbd8d4303
sstrac <s.r.strachan@outlook.com>
2020-11-10 01:08:06 +0000

    Complete texture burnt ice block

--- main/java/io/github/srstr/ringsmod/block/BurntIceBlock.java
+++ main/java/io/github/srstr/ringsmod/block/BurntIceBlock.java
@@ -1,11 +1,17 @@
 package io.github.srstr.ringsmod.block;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BreakableBlock;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-
-public class BurntIceBlock extends Block {
+public class BurntIceBlock extends BreakableBlock {
     private BurntIceBlock() {
-        super(Properties.create(Material.ICE).hardnessAndResistance(1000000));
+        super(Properties.create(Material.GLASS)
+                .slipperiness(0.98F)
+                .tickRandomly()
+                .hardnessAndResistance(10000.0F)
+                .sound(SoundType.GLASS)
+                .notSolid()
+        );
     }
 
     public static BurntIceBlock create(){
